4930480dd82e9e2bc9f2cd07c11cc7905e41b783
Qonus <a1batkonus1@gmail.com>
2026-08-19 09:49:01 +0500

    Add Username constraints

---
+++ README.md
@@ -0,0 +1,2 @@
+## Symfony admin panel
+
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,21 +4,35 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new Length(
+                        min: 3,
+                        max: 30,
+                        minMessage: 'Your username must be at least {{ limit }} characters long.',
+                        maxMessage: 'Your username cannot be longer than {{ limit }} characters.',
+                    ),
+                    new Regex(
+                        pattern: '/^[a-zA-Z0-9_-]+$/',
+                        message: 'Your username can only contain digits, letters, underscores, or hyphens.',
+                    ),
+                ],
+            ])
             ->add('email', EmailType::class, [
                 'required' => true,
             ])
